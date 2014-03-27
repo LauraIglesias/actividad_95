fb36d664085bd3f2d2b7b98f375113d7f6a57b83
Laura <lalacdll@hotmail.com>
2014-03-27 19:20:30 +0100

    modificado constructor de clase responder, poniendo un hashMap para unir palabras con frases de respuesta

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -10,18 +11,24 @@ import java.util.Random;
 public class Responder
 {
     ArrayList <String> respuestas;
+     HashMap<String,String> palabrasClave;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
         respuestas = new ArrayList<>();
+        palabrasClave = new HashMap<>();
         respuestas.add("¿Has reiniciado el sistema?");
         respuestas.add("Te ha surgido algun fallo");
         respuestas.add("¿Que error le sale por pantalla?");
         respuestas.add("Actualice la aplicacion");
         respuestas.add("Me puede describir mas concretamente su error");
         respuestas.add("Espere un momento que voy a consultarlo");
+        palabrasClave.put("apaga",respuestas.get(2));
+        palabrasClave.put("abrir",respuestas.get(3));
+        palabrasClave.put("hola",respuestas.get(1));
+        palabrasClave.put("no",respuestas.get(6));
     }
 
     /**
